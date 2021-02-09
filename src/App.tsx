657ba4113c51f4ead0bd1e6c23b9dc707965ba6b
Sam Buckley <samuel.stbrn@gmail.com>
2021-02-09 13:44:03 +0000

    linked mock data to show available users

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React, { useState } from "react";
-import Welcome from "./pages/Welcome/Welcome";
-import Matches from "./pages/Matches/Matches";
-import firebase, { provider } from "./firebase";
-import {
-  HashRouter as Router,
-  Route,
-  Link,
-  Switch,
-  useHistory,
-} from "react-router-dom";
+import Routes from "./routes/Routes";
 
 const App: React.FC = () => {
-  const [user, setUser] = useState<firebase.User | undefined>();
-
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/matches" component={Matches} />
-      </Switch>
-    </Router>
-  );
+  return <Routes />;
 };
 
 export default App;
