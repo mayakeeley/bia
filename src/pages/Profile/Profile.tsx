ec36cad88a7001ceef66905b58c319d3991df1fa
Kunmi Williams <kumswilliams@gmail.com>
2021-02-19 09:36:43 +0000

    working top down

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -12,26 +12,49 @@ import mockdata from "../../assets/mockData/MockData";
 import { UserModel } from "../../models/user.model";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import SettingsIcon from "@material-ui/icons/Settings";
-import { lavenderBlush, jordyBlue, azalea, mauvelous, springRain } from "theme";
+import {
+  lavenderBlush,
+  jordyBlue,
+  azalea,
+  mauvelous,
+  springRain,
+  white,
+} from "theme";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    profile: {
+    profileWrapper: {
       backgroundColor: mauvelous,
-      height: "20rem",
+      height: "100%",
+    },
+    white: {
+      color: white,
+    },
+    profileTitle: {
+      display: "flex",
+      alignItems: "center",
+    },
+    profile: {},
+    icon: {
+      marginLeft: "0.5rem",
+      color: white,
+    },
+    profileHeader: {
+      display: "flex",
+      justifyContent: "space-between",
       padding: "2rem",
-      position: "relative",
     },
 
     profilePill: {
       backgroundColor: jordyBlue,
       position: "absolute",
-      top: "6rem",
-      left: "4rem",
-      right: "4rem",
+      top: "0rem",
+      left: "0rem",
+
       borderRadius: "2rem",
       width: "15rem",
       color: "white",
+      zIndex: theme.zIndex.tooltip,
     },
     profilePillText: {
       display: "flex",
@@ -44,13 +67,12 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     profileInfo: {
       backgroundColor: lavenderBlush,
-      position: "absolute",
+      position: "relative",
       top: "10rem",
       left: "0",
       width: "100%",
       height: "28rem",
       borderRadius: "2.5rem",
-      zIndex: "-moz-initial",
     },
 
     profileBody: {
@@ -63,42 +85,42 @@ const Profile: React.FC<{ user: UserModel }> = ({ user }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.profile}>
-      <Grid
-        container
-        direction="row"
-        justify="space-between"
-        align-items="flex-start"
-      >
-        <Typography variant="h3" color="initial">
-          Profile
-        </Typography>
-        <AccountCircleIcon color="primary" />
-        <SettingsIcon />
-      </Grid>
-      <section className={classes.profilePill}>
-        <Avatar
-          className={classes.profileIcon}
-          src={users[0].photoUrl}
-        ></Avatar>
-        <div className={classes.profilePillText}>
-          <Typography variant="h3"> {users[0].name} </Typography>
-          <Typography variant="h3">{users[0].location}</Typography>
-          <Typography variant="h4">{users[0].dob}</Typography>
-        </div>
-      </section>
-      <main className={classes.profileInfo}>
-        <Typography variant="h4"> About</Typography>
-        <Typography variant="body1" className={classes.profileBody}>
-          {users[0].about}
-        </Typography>
-        <Typography variant="h4"> Activities</Typography>
+    <div className={classes.profileWrapper}>
+      <div className={classes.profile}>
+        <div className={classes.profileHeader}>
+          <div className={classes.profileTitle}>
+            <Typography className={classes.white} variant="h3">
+              Profile
+            </Typography>
+            <AccountCircleIcon className={classes.icon} />
+          </div>
 
-        <Typography variant="h4"> Goals</Typography>
-        <Typography variant="body1" className={classes.profileBody}>
-          {users[0].goals}
-        </Typography>
-      </main>
+          <SettingsIcon />
+        </div>
+        <main className={classes.profileInfo}>
+          <section className={classes.profilePill}>
+            <Avatar
+              className={classes.profileIcon}
+              src={users[0].photoUrl}
+            ></Avatar>
+            <div className={classes.profilePillText}>
+              <Typography variant="h3"> {users[0].name} </Typography>
+              <Typography variant="h3">{users[0].location}</Typography>
+              <Typography variant="h4">{users[0].dob}</Typography>
+            </div>
+          </section>
+          <Typography variant="h4"> About</Typography>
+          <Typography variant="body1" className={classes.profileBody}>
+            {users[0].about}
+          </Typography>
+          <Typography variant="h4"> Activities</Typography>
+
+          <Typography variant="h4"> Goals</Typography>
+          <Typography variant="body1" className={classes.profileBody}>
+            {users[0].goals}
+          </Typography>
+        </main>
+      </div>
       <NavBar />
     </div>
   );
