7a42b12dd2a211cfaa3e827551d2ffae4553ee38
maya <maya.keeley@outlook.com>
2021-05-01 22:17:20 +0100

    app merging issues fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import { UserModel } from "models/user.model";
 import * as React from "react";
-import { useState } from "react";
-import { BiaUserContext } from "./AppContext";
+import Container from "@material-ui/core/Container";
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
 import Routes from "./routes/Routes";
+import { azalea } from "theme";
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        appWrapper: {
+            maxWidth: "768px",
+            maxHeight: "1024px",
+            padding: 0,
+        },
+        nonMobileWrapper: {
+            minWidth: "100vw",
+            minHeight: "100vh",
+            backgroundColor: azalea,
+            padding: 0,
+            display: "flex",
+            justifyContent: "flex-end",
+        },
+    })
+);
 const App: React.FC = () => {
-    const [biaUser, setBiaUser] = useState<UserModel>();
-
+    const classes = useStyles();
     return (
-        <BiaUserContext.Provider value={{ biaUser, setBiaUser }}>
-            <Routes />
-        </BiaUserContext.Provider>
+        <Container className={classes.nonMobileWrapper}>
+            <Container className={classes.appWrapper}>
+                <Routes />
+            </Container>
+        </Container>
     );
 };
 
